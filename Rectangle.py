51eb501acdc184ba50a8e965719f9690699fd974
ConcernedFox <vihaan.gururani@gmail.com>
2025-09-07 09:57:33 -0400

    Add files via upload

---
+++ Rectangle.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((600,800))
+pygame.display.set_caption("Rectangles, One Red One Green")\
+class Vihaan():
+    name = "Vihaan"
+    age = 10
+    def __init__(self, color, dimension):
+        
+Rect = True
+while Rect == True:
+    for P in pygame.event.get():
+        if P.type == pygame.QUIT:
+            Rect = False
