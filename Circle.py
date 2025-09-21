7a3a557f9d9d07720d93dcc9eb0c49f9319b135d
ConcernedFox <vihaan.gururani@gmail.com>
2025-09-21 09:42:05 -0400

    Add files via upload

--- Circle.py
+++ Circle.py
@@ -6,14 +6,27 @@ class Circle():
         self.color = input("What color do you want in the circle?")
         self.x = 250.5
         self.y = 250.5
-        self.radius = int(input("What number do you want from 1 to 10"))
-        self.width = int(input("What number do you want from 1 to 10"))
+        self.radius = 100
+        self.width = 100
     def draw(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, self.width)        
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, self.width) 
+    def draw_again(self):
+        choice = input("Do you want a bigger circle?")
+        if choice == "yes":
+            pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius + self.radius, self.width)
+        elif choice == "no":
+            pygame.quit
+        else:
+            choice = input("Try again. Do you want a bigger circle? yes, or no")
 Vihaan_G = Circle()
-Vihaan_G.draw()
-pygame.display.update()
+#Vihaan_G.draw_again()
 while True:
     for p in pygame.event.get():
         if p.type == pygame.QUIT:
-            pygame.quit()   
+            pygame.quit()  
+        elif p.type == pygame.MOUSEBUTTONDOWN:
+            Vihaan_G.draw()
+            pygame.display.update()
+        elif p.type == pygame.MOUSEBUTTONUP:
+            Vihaan_G.draw_again()
+            pygame.display.update()
